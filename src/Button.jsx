3ebf2cd85929fb8b80c5e761fea395dc79e056df
Rahul <parmarrahulh13@gmail.com>
2026-08-06 22:28:44 +0530

    learn event handling

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,26 +1,23 @@
-import { useState } from 'react'
-
-
+import { useState } from "react";
 
 function Button() {
-     const [numbers] = useState({
-    firstnumber: 10,
-    secondnumber: 5
-  });
-  
- const [answer,setanswer]=useState(0);
+
+  const [count, setCount] = useState(0);
+
   return (
     <>
-        <div className="buttons">
-            <p className='firstnumber'>firstnumber 10</p><br />
-            <p className='secondnumber'>secondnumber 5</p><br />
-            <p className='secondnumber'>10 + 5 </p>
-            <button className='sum' onClick={()=>setanswer(numbers.firstnumber+numbers.secondnumber)}>sum</button><br />
-            <p>Answer is {answer}</p>
-        </div>
+      <p>{count}</p>
 
+      <button
+        onClick={() => setCount(count + 1)}
+      >
+        +
+      </button>
     </>
-  )
+  );
 }
 
 export default Button
+
+
+
